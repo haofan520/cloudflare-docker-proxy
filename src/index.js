d8a4d52502c16d62471a9fa63cd5a42ff2e6bf60
haofan520 <62988858+haofan520@users.noreply.github.com>
2024-06-26 15:50:53 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,17 +7,13 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
-  "ecr.libcuda.so": "https://public.ecr.aws",
-
-  // staging
-  "docker-staging.libcuda.so": dockerHub,
+  "docker.registry.kang141841013.eu.org": "https://registry-1.docker.io",
+  "quay.registry.kang141841013.eu.org": "https://quay.io",
+  "gcr.registry.kang141841013.eu.org": "https://gcr.io",
+  "k8s-gcr.registry.kang141841013.eu.org": "https://k8s.gcr.io",
+  "k8s.registry.kang141841013.eu.org": "https://registry.k8s.io",
+  "ghcr.registry.kang141841013.eu.org": "https://ghcr.io",
+  "cloudsmith.registry.kang141841013.eu.org": "https://docker.cloudsmith.io"
 };
 
 function routeByHosts(host) {
